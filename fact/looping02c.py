f08a77c2d850027314d70fdbe1e09a908b5171c3
Mayuridalavai <mayuri.gj@gmail.com>
2022-09-30 17:56:57 +0000

    Looping techniques

---
+++ fact/looping02c.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   For - Looping across a file opened with 'with'
+         while also being gentle on memory consumption."""
+
+def main():
+    # open file in read mode
+    with open("dnsservers.txt", "r") as dnsfile:
+         # indent to keep the dnsfile object open
+         # loop across the lines in the file
+          for svr in dnsfile:
+              #print and end without a newline
+               print(svr, end="")
+
+main()
+
