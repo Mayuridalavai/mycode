921d59767a2d54f2ac66a91741fb2c2805af775c
Mayuridalavai <mayuri.gj@gmail.com>
2022-10-03 16:49:53 +0000

    Python and physical interfaces

--- netfacd/interface_reader.py
+++ netfacd/interface_reader.py
@@ -14,3 +14,7 @@ for i in netifaces.interfaces():
     except:
         print('Could not collect adapter information')
 
+i = input("what adaptor would you like to obtain the MAC address from?")
+print((netifaces.ifaddresses(i)[netifaces.AF_LINK])[0]['addr'])
+i = input("What adaptor would you like to abtain the IP addtess from?")
+print((netifaces.ifaddresses(i)[netifaces.AF_INET])[0]['addr'])
